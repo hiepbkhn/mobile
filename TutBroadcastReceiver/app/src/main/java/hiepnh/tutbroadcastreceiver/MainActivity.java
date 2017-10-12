0794dda4dab0a4d0de795f13ccac6f2e5f570887
Hiep H. Nguyen <hiepnh.vtt@gmail.com>
2017-10-12 10:12:13 +0700

    Broadcast Receiver + Content Provider

---
+++ TutBroadcastReceiver/app/src/main/java/hiepnh/tutbroadcastreceiver/MainActivity.java
@@ -0,0 +1,23 @@
+package hiepnh.tutbroadcastreceiver;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // broadcast a custom intent.
+
+    public void broadcastIntent(View view){
+        Intent intent = new Intent();
+        intent.setAction("hiepnh.CUSTOM_INTENT");
+        sendBroadcast(intent);
+    }
+}
